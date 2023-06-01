139248f9315fb78becda2379a1bf29400456722e
dawidahs <46559351+dawidahs@users.noreply.github.com>
2023-06-01 11:03:32 +0200

    Update FileMerger_leftJoin_toXLS.py

--- FileMerger_leftJoin_toXLS.py
+++ FileMerger_leftJoin_toXLS.py
@@ -8,4 +8,4 @@ df2 = pd.read_csv('UserIDvsPayout.csv')
 merged_left = pd.merge(left=df1, right=df2, how='left', left_on='USERID', right_on='USERID')
 
 #Save to excel format with UTF-8 encoding
-merged_left.to_excel('C:\\Users\\david\\Desktop\\TEMP\\Final_SalaryReview_20200529_1600_wBonus.xlsx', index=False, encoding='utf-8')
+merged_left.to_excel('\Final_SalaryReview_20200529_1600_wBonus.xlsx', index=False, encoding='utf-8')
